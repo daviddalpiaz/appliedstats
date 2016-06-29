69fc19a81ec456cd842ea10b787745699d152b1c
David Dalpiaz <dalpiaz2@illinois.edu>
2016-06-28 19:12:32 -0500

    add package

--- r_book.R
+++ r_book.R
@@ -1,6 +1,6 @@
 ## @knitr load_packages
 # Any package that is required by the script below is given here:
-inst_pkgs = load_pkgs =  c("ggplot2","devtools","zoo")
+inst_pkgs = load_pkgs =  c("ggplot2","devtools","zoo", "plot3D")
 inst_pkgs = inst_pkgs[!(inst_pkgs %in% installed.packages()[,"Package"])]
 if(length(inst_pkgs)) install.packages(inst_pkgs)
 
