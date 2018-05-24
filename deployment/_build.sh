e546137a0b7ef6785a95af44fe088263bb47b56f
daviddalpiaz <dalpiaz2@illinois.edu>
2018-05-24 11:56:08 -0500

    hmmm. trying an alternative approach.

--- deployment/_build.sh
+++ deployment/_build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env Rscript
 
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
-bookdown::render_book("index.Rmd", "bookdown::pdf_book")
+bookdown::render_book("index.Rmd", "bookdown::gitbook", new_session = TRUE)
+bookdown::render_book("index.Rmd", "bookdown::pdf_book", new_session = TRUE)
